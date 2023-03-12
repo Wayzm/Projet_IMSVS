46036c9dc1a7bdee9ea86698b9f882f3effddd78
ggdoe <gregoire.doebele@ens-paris-saclay.fr>
2023-03-12 22:44:23 +0100

    ca marche a peu près

--- projet3.R
+++ projet3.R
@@ -8,9 +8,10 @@ x_max = 50
 DThreshold = 50
 IThreshold = 5
 t = 0
-lyso_time = 5
-
-# Make it possible to be divided by 3
+lyso_time = 10
+init_food_value = 10
+food_per_itt = 250
+timestamp = 40
 n_bacts = 30
 
 # Placing  bacteria
@@ -20,24 +21,21 @@ coords <<- matrix(data = 0, ncol = n_bacts, nrow = 2)
 Health <<- matrix(data = 0, ncol =  x_max, nrow = y_max)
 Type <<- matrix(data = 0, ncol =  x_max, nrow = y_max)
 
-iter = 1
-while(iter <= n_bacts){
-  ii = sample(1:x_max, 1, replace = TRUE)
-  jj = sample(1:y_max, 1, replace = TRUE)
-  x = ii[1]
-  y = jj[1]
-  if(Type[x, y] == 0){
-    coords[, iter] <<- c(x, y)
-    iter = iter + 1
+id = sample(1:(x_max*y_max), n_bacts, replace = FALSE)
+
+  for (i in 1:n_bacts) {
+    x = ((id[i]-1) %% y_max) + 1
+    y = floor((id[i]-1) / y_max) + 1
+    coords[, i] <<- c(x, y)
+    i = i + 1
     Health[x,y] <<- 20
     rd = runif(1)
     if(rd < 0.33)      Type[x,y] <<- 1 # 'B'
     else if(rd < 0.66) Type[x,y] <<- 2 # 'C'
     else               Type[x,y] <<- 3 # 'D'
-    
   }
 }
-}
+
 Init_bacteria(n_bacts)
 color = diag(Type[coords[1,],coords[2,]])
 plot(coords[1,],coords[2,], xlim = c(1, x_max), ylim = c(1, y_max), col=color)
@@ -53,7 +51,7 @@ Placing_food<-function(n_food)
     for(j in 1:x_max)
     {
       vec = runif(1)
-      Food_Value[i, j] <<- 20
+      Food_Value[i, j] <<- init_food_value
       
       if(vec[1] < 0.16)
         Food[i, j] <<- "I"
@@ -73,15 +71,14 @@ Placing_food<-function(n_food)
 
 Add_food<-function(n_food)
 {
-xx = sample(1:x_max, n_food,replace = FALSE)
-yy = sample(1:y_max, n_food,replace = FALSE)
+  id = sample(1:(x_max*y_max), n_food, replace = FALSE)
   
   for(ii in 1:n_food)
   {
     vec = runif(1)
-    i = xx[ii]
-    j = yy[ii]
-    Food_Value[i, j] <<- 20
+    i = ((id[ii]-1) %% y_max) + 1
+    j = floor((id[ii]-1) / y_max) + 1
+    Food_Value[i, j] <<- init_food_value
     
     if(vec[1] < 0.16)
       Food[i, j] <<- "I"
@@ -102,7 +99,7 @@ yy = sample(1:y_max, n_food,replace = FALSE)
 Cell_Division<-function(x, y, division_probability)
 {
 p = division_probability
-original_health = Health[x, y] / 2
+original_health = floor(Health[x, y] / 2)
 new_cell_health = original_health * p
 typeb<-Type[x, y]
 fin = 0
@@ -129,10 +126,13 @@ cells_coords <- matrix(data = 0, ncol =  8, nrow = 2)
     }
     counter = counter + 1
   }
-  if(empty_cells >= 2){
-    rd = sample(1:empty_cells, 2, replace = FALSE) # On ne veut pas 2 fois la même position du coup
-    for(idx in 1:2){
-      index = rd[idx]
+  if(empty_cells > 0)
+  {
+    if (empty_cells >= 2)
+      rd = sample(1:empty_cells, 2, replace = FALSE) # On ne veut pas 2 fois la même position du coup
+    else
+      rd = 1
+    for(index in rd){
       new_cell_x = cells_coords[1, index]
       new_cell_y = cells_coords[2, index]
       Health[new_cell_x, new_cell_y] <<- original_health
@@ -141,13 +141,15 @@ cells_coords <- matrix(data = 0, ncol =  8, nrow = 2)
       coords <<- cbind(coords, c(new_cell_x, new_cell_y))
     }
   }
+    
   # remove bacterie // Bah du coup non?
   Kill_bacteria(x,y)
 }
 
 Comsumption<-function(x, y)
 {
-  r = 0
+  tmp <- 0
+  if(x > 1 && x < x_max && y > 1 && y < y_max)
   for(j in x-1:x+1)
   {
     for(k in y-1:y+1)
@@ -159,23 +161,24 @@ Comsumption<-function(x, y)
         Type[x, y] == 2 && (Food[j, k] == "G" || Food[j, k] == "F" || Food[j, k] == "L" || Food[j, k] == "I")
         ||
         Type[x, y] == 3 && (Food[j, k] == "T" || Food[j, k] == "C")
-        ) && Food_Value[j, k] != 0
+        ) && Food_Value[j, k] >= 1
       )
-  {
+      {
         Food_Value[j, k] <<- Food_Value[j, k] - 1
-        r = r + 1
-        if(Food_Value[j, k] == 0)
+        tmp <- tmp + 1
+        
+        if(Food_Value[j, k] <= 0)
           Food[j, k] <<- 0
       }
     }
   }
-  Health[x, y] <<- Health[x, y] + r
+  Health[x, y] <<- Health[x, y] + tmp
 }
 
 
 Adjust_lysozyme<-function(x, y)
 {
-  if(Health[x, y] > IThreshold  && Type[x, y] == 2){ 
+  if(Type[x, y] == 2){ # Bactérie C
     Health[x, y] <<- Health[x, y] - 2
   }
 }
@@ -187,24 +190,29 @@ Kill_bacteria<-function(x,y){
   b = which(coords[2,] == y)
   index = intersect(a,b)
   
+  Health[x, y] <<- 0
+  Type[x, y] <<- 0
+  
   if(index != 0){
     n_bacts <<- n_bacts - 1
-    Health[x, y] <<- 0
-    Type[x, y] <<- 0
     coords <<- coords[,-index]
   }
 }
-
 
 Init_bacteria(n_bacts)
 Placing_food(x_max * y_max)
 
 
-timestamp = 30
+GIB = rep(NA, times=timestamp)
+num_bacteria = matrix(data = 0, ncol = timestamp, nrow = 3)
+
 for(t in 1:timestamp)
 {
   color = diag(Type[coords[1,],coords[2,]])
   plot(coords[1,],coords[2,], xlim = c(1, x_max), ylim = c(1, y_max), col=color)
+  title(paste("timestamp = ",t))
+  legend(legend = c("B", "C", "D"), col = 1:3, x = "bottomright", lty=1)
+
   for(i in 1:x_max)
   {
     for(j in 1:y_max)
@@ -215,21 +223,49 @@ for(t in 1:timestamp)
       if(Health[i, j] > DThreshold)
         Cell_Division(i, j, 1.0)
       
-      if(Health[i, j] > IThreshold){
-        Health[i, j] = Health[i, j] - IThreshold
-        if(Health[i, j] <= 0)
-          Kill_bacteria(i,j)
-      }
+      Health[i, j] = Health[i, j] - IThreshold
         
       if(t > lyso_time)
         Adjust_lysozyme(i, j)
+      
+      if(Type[i, j] != 0 && Health[i, j] <= 0)
+        Kill_bacteria(i,j)
+      if(Health[i, j] <= 0)
+        Health[i, j] <- 0
     }
   }
-if(t %% 10 == 0){Add_food(50)}
+  
+  Add_food(food_per_itt)
+  
+  B = sum(Type[,] == 1)
+  C = sum(Type[,] == 2)
+  D = sum(Type[,] == 3)
+  Bs = sum(Health[Type[,] == 1])
+  Cs = sum(Health[Type[,] == 2])
+  Ds = sum(Health[Type[,] == 3])
+  
+  GIB[t] = (B/Bs) / ((C+D)/(Cs+Ds)) - 1
+  num_bacteria[,t] = c(B,C,D) 
 }
 
-# plot
-color = diag(Type[coords[1,],coords[2,]])
-plot(coords[1,],coords[2,], xlim = c(1, x_max), ylim = c(1, y_max), col=color)
+ts = 1:timestamp
 
+plot(ts, c(num_bacteria[1,]), type = "o", col = 1, ylim = c(40, max(num_bacteria)))
+lines(ts, num_bacteria[2,], type = "o", col = 2)
+lines(ts, num_bacteria[3,], type = "o", col = 3)
+abline(v=lyso_time)
+legend(legend = c("B", "C", "D"), col = 1:3, lty=1, x = "topright")
+
+up = GIB>=0
+down = GIB<=0
+half = (max(ts[up]) + min(ts[down]))/2
+plot(ts, GIB, type = "o")
+polygon(x = c(min(ts[up]), ts[up],half),
+        y = c(0, GIB[up],0),
+        col = "red" )
+polygon(x = c(half,ts[down],max(ts[down])),
+        y = c(0,GIB[down],0),
+        col = "blue" )
+lines(1:timestamp, rep(0,timestamp))
+abline(v=lyso_time)
 
